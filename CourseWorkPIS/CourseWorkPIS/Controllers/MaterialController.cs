290655954e8c00ac5f61278b554cc5666a433e84
monuelDePari <roman.lynnyk.sa@gmail.com>
2019-12-05 01:23:48 +0200

    Created Asp.net project and added tabs in nav menu

---
+++ CourseWorkPIS/CourseWorkPIS/Controllers/MaterialController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace CourseWorkPIS.Controllers
+{
+    public class MaterialController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
