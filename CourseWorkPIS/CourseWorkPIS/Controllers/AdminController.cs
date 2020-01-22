00156bb08607c9ab656e75bf617ec0c5591bb150
monuelDePari <roman.lynnyk.sa@gmail.com>
2020-01-22 20:08:22 +0200

    identity work

---
+++ CourseWorkPIS/CourseWorkPIS/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseWorkPIS.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseWorkPIS.Controllers
+{
+    public class AdminController : Controller
+    {
+        UserManager<IdentityUser> _userManager;
+        public AdminController(UserManager<IdentityUser> manager)
+        {
+            _userManager = manager;
+        }
+        public IActionResult Index()
+        {
+            return View(_userManager.Users.ToList());
+        }
+    }
+}
--- CourseWorkPIS/CourseWorkPIS/Models/Course.cs
+++ CourseWorkPIS/CourseWorkPIS/Models/Course.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -11,7 +12,6 @@ namespace CourseWorkPIS.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-
         [Display(Name = "Start Date")]
         [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
